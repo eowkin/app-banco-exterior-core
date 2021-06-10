16e250f45d0b3f4e14c8257b5772dd3e0c7805a2
eowkin <eowkin@gmail.com>
2021-06-10 13:18:19 -0400

    listo url t filtros y los cambios enviados correo hoy jueves2

--- src/main/java/com/bancoexterior/app/convenio/apiRest/MovimientosApiRestImpl.java
+++ src/main/java/com/bancoexterior/app/convenio/apiRest/MovimientosApiRestImpl.java
@@ -50,7 +50,7 @@ public class MovimientosApiRestImpl implements IMovimientosApiRest{
 		//https://172.19.148.51:8443/api/des/V1/parametros/monedas/
 		//https://172.19.148.51:8443/api/des/V1/parametros/monedas/
 		//wsrequest.setUrl("http://172.19.148.48:7108/api/des/V1/parametros/monedas/consultas"); montoDivisa desc
-		wsrequest.setUrl("https://172.19.148.51:8443/api/des/V1/divisas/consultasmovimientos?sort=tasaCliente,desc&sort=montoDivisa,desc");
+		wsrequest.setUrl("https://172.19.148.51:8443/api/des/V1/divisas/consultasmovimientos?sort=codMoneda,desc&sort=tasaCliente,asc&sort=montoDivisa,asc");
 			
 		//retorno: WSResponse [statusText=, status=200, body={"resultado":{"codigo":"0000","descripcion":"Operacion Exitosa."},"monedas":[{"codMoneda":"EUR","descripcion":"EURO Europa","codAlterno":"222","flagActivo":true,"codUsuario":"E33333","fechaModificacion":"2021-05-07 21:24:07"}]}, exitoso=true, httpRetorno=kong.unirest.StringResponse@7451891e, httpError=null, error=null, idConstructor=1]
 		log.info("antes de llamarte WS en consultar");
@@ -113,7 +113,7 @@ public class MovimientosApiRestImpl implements IMovimientosApiRest{
 		//https://172.19.148.51:8443/api/des/V1/parametros/monedas/
 		//https://172.19.148.51:8443/api/des/V1/parametros/monedas/
 		//wsrequest.setUrl("http://172.19.148.48:7108/api/des/V1/parametros/monedas/consultas"); ASC montoDivisa
-		wsrequest.setUrl("https://172.19.148.51:8443/api/des/V1/divisas/consultasmovimientos?sort=tasaCliente,asc&sort=montoDivisa,desc");
+		wsrequest.setUrl("https://172.19.148.51:8443/api/des/V1/divisas/consultasmovimientos?sort=codMoneda,desc&sort=tasaCliente,desc&sort=montoDivisa,desc");
 			
 		//retorno: WSResponse [statusText=, status=200, body={"resultado":{"codigo":"0000","descripcion":"Operacion Exitosa."},"monedas":[{"codMoneda":"EUR","descripcion":"EURO Europa","codAlterno":"222","flagActivo":true,"codUsuario":"E33333","fechaModificacion":"2021-05-07 21:24:07"}]}, exitoso=true, httpRetorno=kong.unirest.StringResponse@7451891e, httpError=null, error=null, idConstructor=1]
 		log.info("antes de llamarte WS en consultar");
